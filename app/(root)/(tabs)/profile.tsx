64e88ad2c66757df626e4a542b4ae380ae5fec3a
Anteneh Atre <anteneh.atre@gmail.com>
2024-09-04 18:05:26 +0300

    Building the welcome or on boarding screen

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+
+const Profile = () => {
+  return (
+    <SafeAreaView>
+        <Text>Profile Screen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Profile
