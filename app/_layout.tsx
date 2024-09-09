d5bed59e3b5cde5cbe0fb6b8a905bf0208047fac
Anteneh Atre <anteneh.atre@gmail.com>
2024-09-09 09:50:50 +0300

    building google maps, google sign in, stripe payment, profile page and other...

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,11 +5,15 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
+import { LogBox } from "react-native";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+LogBox.ignoreLogs(["Clerk:"])
+
 
 if (!publishableKey) {
   throw new Error(
